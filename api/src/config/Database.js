5d495b3aad410d9548c1b76874fec93ac6334df1
sistemasatol <sistemas@atolincorporadora.com.br>
2024-11-11 09:43:26 -0300

    Configurar Database

---
+++ api/src/config/Database.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+
+const database = new Sequelize('projetoAtol', 'root', '*Hksjj2105', {
+      host: 'localhost',
+      dialect: 'mysql'
+});
+database.sync({ force: false }) 
+      .then(() => {
+            console.log("Sincronização com o banco de dados realizada com sucesso!");
+      })
+      .catch((err) => {
+            console.error("Erro ao sincronizar com o banco de dados:", err);
+      });
+
+
+
+module.exports = database;
