8f35ffcf90bcc33ad19f7e5282067b6afddc344a
sistemasatol <sistemas@atolincorporadora.com.br>
2024-11-11 09:43:17 -0300

    Configurar Express e Server

--- api/server.js
+++ api/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-
+const routes = require('./src/router');
 
 const app = express();
 
 app.use(express.json());
-
 app.use(cors());
+app.use(routes);
 
 app.listen(2105, () => {
-      console.log("Servidor rodando na porta 2105");
+  console.log("Servidor rodando na porta 2105");
 });
